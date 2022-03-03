f708854e74100fc28247db64be2b6c78b3eea144
Leth0kuhle <100874469+Leth0kuhle@users.noreply.github.com>
2022-03-03 21:54:17 +0200

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Leth0kuhle
+- 👀 I’m interested in data analysis 
+- 🌱 I’m currently learning finance 
+- 💞️ I’m looking to collaborate on computer sciences programmers 
+- 📫 How to reach me email me 
+
+<!---
+Leth0kuhle/Leth0kuhle is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
